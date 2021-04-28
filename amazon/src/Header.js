fb5e66c1f6520d5dcad131513a835f56f4f1055d
Muhammad Asadullah <muhammad.asad2020@gmail.com>
2021-04-28 12:20:00 -0700

    Navbar Completed
    Navbar Completed

--- amazon/src/Header.js
+++ amazon/src/Header.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Header.css";
 import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 
 function Header() {
   return (
@@ -20,20 +21,27 @@ function Header() {
       <div className="header__nav">
         <Link to="/login" className="header__link">
           <div className="header__option">
-            <span>Hello Asad</span>
-            <span>Sign In</span>
+            <span className="header__optionLineOne">Hello Asad</span>
+            <span className="header__optionLineTwo">Sign In</span>
           </div>
         </Link>
         <Link to="/" className="header__link">
           <div className="header__option">
-            <span>Returns</span>
-            <span>& Orders</span>
+            <span className="header__optionLineOne">Returns</span>
+            <span className="header__optionLineTwo">& Orders</span>
           </div>
         </Link>
         <Link to="/" className="header__link">
           <div className="header__option">
-            <span>Your</span>
-            <span>Prime</span>
+            <span className="header__optionLineOne">Your</span>
+            <span className="header__optionLineTwo">Prime</span>
+          </div>
+        </Link>
+
+        <Link to="/checkout" className="header__link">
+          <div className="header__optionBasket">
+            <ShoppingBasketIcon />
+            <span className="header_optionLineTwo header_basketCount">0</span>
           </div>
         </Link>
       </div>
